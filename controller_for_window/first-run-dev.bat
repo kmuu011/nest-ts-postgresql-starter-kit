6440c208a821c0abf02f4ab9ab7382d4a5263670
Hikari <kmuu011@naver.com>
2025-09-01 21:50:53 +0900

    원클릭 dev 환경 구성 bat 구현

--- controller_for_window/first-run-dev.bat
+++ controller_for_window/first-run-dev.bat
@@ -2,4 +2,17 @@ cd ..
 cd docker-compose
 cd nest_ts_postgresql_starter_kit_dev
 
-docker compose up -d & pause
+docker compose up postgresql -d
+
+timeout /t 2 /nobreak >NUL
+
+cd ..
+cd ..
+cd controller_for_window
+call prisma-deploy.bat && ^
+cd .. && ^
+cd controller_for_window && ^
+call build-server-image.bat && ^
+cd docker-compose && ^
+cd nest_ts_postgresql_starter_kit_dev && ^
+docker compose up -d && pause
